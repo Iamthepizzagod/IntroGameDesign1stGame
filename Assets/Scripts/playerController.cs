2429ea0b28126be00e43e3395742fbdb8591063e
General Lab <Genlab@students.beckercollege.edu>
2018-09-20 20:39:01 -0400

    End of first class progress

---
+++ Assets/Scripts/playerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+    private int playerHealth = 100;
+    public float playerSpeed;
+    private Rigidbody2D myRigidBody;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetAxisRaw("Horizontal") > 0f)
+        {
+            myRigidBody.velocity = new Vector2(playerSpeed, myRigidBody.velocity.y);
+
+
+        }
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            myRigidBody.velocity = new Vector2(playerSpeed*-1, myRigidBody.velocity.y);
+
+        }
+
+        if (Input.GetAxisRaw("Vertical") > 0f)
+        {
+            myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, playerSpeed);
+        }
+    }
+}
+
+
